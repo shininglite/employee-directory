bc53df049d63b84bba8cc2dc7548e9f5132904ca
shininglite <vandeusent@gmail.com>
2020-05-02 20:42:18 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import Table from './components/Table';
+// import React from 'react';
+// //import logo from './logo.svg';
+// import './App.css';
+// import './components/EmployeeContainer';
 
+// function App() {
+//   return (
+//     <div>
+//       return <EmployeeContainer />;
+//     </div>
+//   );
+// }
+
+import React from "react";
+import EmployeeContainer from "./components/EmployeeContainer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <div className="App">
-      <Header />
-      <Search />
-      <Table />
-      </div>
-      </header>
-    </div>
-  );
+  return <EmployeeContainer />;
 }
 
 export default App;
