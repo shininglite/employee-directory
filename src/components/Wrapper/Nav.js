2aaffe884792ceef66558ea0f7cdc3804b386b70
shininglite <vandeusent@gmail.com>
2020-05-10 15:29:09 -0500

    add wrapper components

---
+++ src/components/Wrapper/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SearchBox from "./SearchBox.js";
+import "../styles/Nav.css";
+
+function Nav({ handleSearchChange }) {
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light">
+      <div className="navbar-collapse row" id="navbarNav">
+        <SearchBox handleSearchChange={handleSearchChange} />
+      </div>
+    </nav>
+  );
+}
+export default Nav;
