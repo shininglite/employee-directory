b2c70169483fdd2819f6b945273f2e8dcf9d1e00
shininglite <vandeusent@gmail.com>
2020-05-10 16:54:10 -0500

    change message below Employee Directory heading

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <p>Mouse over column headings and click carets to sort, or filter directory using the letters or numbers in the search box to narrow your results.</p>
       </div>
     )
   }
