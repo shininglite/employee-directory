7cb7be4a2030b1751389bc2c4ad5ae134111ac1b
shininglite <vandeusent@gmail.com>
2020-05-10 16:23:23 -0500

    reorganize files to be in correct folders

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import "../styles/Header.css";
 
-function Header(props){
-  return (
-    <div>
-      <h1>Employee Directory</h1>
-    </div>
-  );
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+      </div>
+    )
+  }
 }
-
-export default Header;
