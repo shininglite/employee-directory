b0ef259e7a6a8b4c2bc61123cd9ace3804255a58
shininglite <vandeusent@gmail.com>
2020-05-10 15:02:49 -0500

    add wrapper, header and main to App div in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-// import React from 'react';
-// //import logo from './logo.svg';
-// import './App.css';
-// import './components/EmployeeContainer';
-
-// function App() {
-//   return (
-//     <div>
-//       return <EmployeeContainer />;
-//     </div>
-//   );
-// }
-
 import React from "react";
-import EmployeeContainer from "./components/EmployeeContainer";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
+import "./App.css";
 
 function App() {
-  return <EmployeeContainer />;
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
